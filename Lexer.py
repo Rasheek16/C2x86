26007822546fae217d217eeae6a2ce0aed128f95
Rasheek <sallanrasheek@gmail.com>
2024-12-15 13:10:05 +0000

    created a lexer and compiler driver to tokenize simple programs

--- Lexer.py
+++ Lexer.py
@@ -1,9 +1,28 @@
-import os 
 
-# READ THE SOURCE FILE AND PRODUCE A LIST OF TOKENS 
-def read_file(input_file):
-    data=f.read()
-    
-    
-    
-    
+from Tokens import patterns 
+import re 
+
+
+def lex(data):
+    tokens=[]
+    while data:
+        if data[0].isspace():
+            data=data.lstrip()
+            
+        else:
+            match_found = False
+            for token_type , pattern in patterns:
+                print(token_type,pattern)
+                isMatch = re.match(pattern,data)
+                print(isMatch)
+                if isMatch:
+                    token_value = isMatch.group(0)
+                    tokens.append((token_type,token_value))
+                    match_found = True 
+                    data = data[len(token_value):]
+            if not match_found:
+                raise ValueError('Unexpected inputs')
+    return tokens 
+
+
+
--- Tokens.py
+++ Tokens.py
@@ -1,12 +1,12 @@
-patterns = {
-    "Identifier": r"[a-zA-Z_]\w*\b",
-    "Constant": r"[0-9]+\b",
-    "int_keyword": r"\bint\b",
-    "void_keyword": r"\bvoid\b",
-    "return_keyword": r"\breturn\b",
-    "Open_parenthesis": r"\(",
-    "Close_parenthesis": r"\)",
-    "Open_brace": r"{",
-    "Close_brace": r"}",
-    "Semicolon": r";"
-}
+patterns = [
+    ("Identifier",r"[a-zA-Z_]\w*\b"),
+     ("Constant", r"[0-9]+\b"),
+     ("int_keyword", r"int\b"),
+     ("void_keyword", r"void\b"),
+     ("return_keyword", r"return\b"),
+     ("Open_parenthesis", r"\("),
+     ("Close_parenthesis", r"\)"),
+     ("Open_brace", r"{"),
+     ("Close_brace", r"}"),
+     ("Semicolon", r";")
+]
