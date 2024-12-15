8bdf71d9379d5cf2cc248f0fca6fe0cb81d43b9c
Rasheek <sallanrasheek@gmail.com>
2024-12-15 12:17:22 +0000

    created a set of tokens

---
+++ Tokens.py
@@ -0,0 +1,12 @@
+patterns = {
+    "Identifier": r"[a-zA-Z_]\w*\b",
+    "Constant": r"[0-9]+\b",
+    "int_keyword": r"\bint\b",
+    "void_keyword": r"\bvoid\b",
+    "return_keyword": r"\breturn\b",
+    "Open_parenthesis": r"\(",
+    "Close_parenthesis": r"\)",
+    "Open_brace": r"{",
+    "Close_brace": r"}",
+    "Semicolon": r";"
+}
